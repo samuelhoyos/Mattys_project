8d4647dbe08b26bd1e2ee71c2d35c9911b910023
Samuel Hoyos Aristizabal <samuel.hoyos@udea.edu.co>
2026-01-19 15:41:51 +0100

    Fitting n max evolution for long and short tfwhm

--- modules/data.py
+++ modules/data.py
@@ -35,3 +35,34 @@ def get_maxima_envelope(
         for times, nmax_profile in zip(times_list, nmax_profiles_list)
     ]
     return np.array(times_nmax_list), np.array(nmax_list)
+
+def get_field(component: str, filename: str, subset_x:list=None,subset_y:list=None, subset_z:list=None) -> np.array:
+    file_path = data_path / filename
+    S = happi.Open(f"{file_path}", verbose=False)
+    if  subset_x is None and subset_y is None and subset_z is None:
+            f = S.Field(0, f"{component}")
+    else:
+        f = S.Field(0, f"{component}", subset={
+            'x': subset_x,
+            'y': subset_y,
+            'z': subset_z
+        })
+    return np.array(f.getData())
+
+def long_time(tfwhm,A,B,C):
+        return (A/((tfwhm+B))) - C
+    
+def short_time(tfwhm,A,B):
+        return A+tfwhm*B
+
+def fit_envelope(tfwhm_list:list,nmax_array: np.ndarray,):
+    mask=list(nmax_array).index(np.max(nmax_array))
+    tfwhm_list_short= np.array(tfwhm_list)[:mask]
+    tfwhm_list_long= np.array(tfwhm_list)[mask:]
+    
+    init_long=[1,0,1]
+    init_short=[0,1]
+    popt_long, pcov_long = curve_fit(long_time, tfwhm_list_long, nmax_array[mask:], p0=init_long,  maxfev = 800000)
+    popt_short, pcov_short = curve_fit(short_time, tfwhm_list_short, nmax_array[:mask], p0=init_short, maxfev = 800000)
+    return popt_long, popt_short
+
--- modules/plot.py
+++ modules/plot.py
@@ -24,6 +24,9 @@ def plot_nmax_profiles(
     plt.plot(
         times_nmax_array * 1e15, nmax_array, linestyle="-", linewidth=3, color="black"
     )
+    plt.plot(
+        times_nmax_array * 1e15, nmax_array, linestyle="-", linewidth=3, color="black"
+    )
     plt.xlabel("$t$ (fs)", fontsize=30)
     plt.ylabel("Max Positron Density", fontsize=30)
     plt.title("Max Positron Density Profiles Over Time", fontsize=30)
@@ -45,4 +48,46 @@ def plot_single_profile(times, nmax_profile, tfwhm):
     plt.title("Max Positron Density Profile Over Time", fontsize=30)
     plt.legend(loc="upper right", fontsize=25, frameon=False)
     plt.tight_layout()
+    plt.show()
+def plot_nmax_tfwhm(tfwhm_list: list, nmax_array: np.ndarray, popt_long: np.ndarray=None, popt_short: np.ndarray=None):
+   
+    mask=list(nmax_array).index(np.max(nmax_array))
+    tfwhm_list_short= np.array(tfwhm_list)[:mask]
+    tfwhm_list_long= np.array(tfwhm_list)[mask:]
+    print(tfwhm_list_long)
+    plt.figure(figsize=(12, 8))
+    plt.plot(
+        list(tfwhm_list),
+        nmax_array,
+        marker='o',
+        linestyle='',
+        color='blue',
+        label='Data'
+    )
+    if popt_long is not None:
+ 
+        plt.plot(
+            tfwhm_list_long,
+            data.long_time(tfwhm_list_long, *popt_long),
+            linestyle='--',
+            linewidth=3,
+            color='red',
+            #label=f'${popt_long[0]}/(t_{{fwhm}}-{popt_long[1]})+{{{popt_long[2]}}}$'
+        )
+    if popt_short is not None:
+        plt.plot(
+            tfwhm_list_short,
+            data.short_time(tfwhm_list_short, *popt_short),
+            linestyle='--',
+            linewidth=3,
+            color='red',
+            #label=f'${popt_short[0]:.2f}+{popt_short[1]:.2f}t_{{fwhm}}$'
+        )
+
+    plt.xlabel("$t_{fwhm}$ (fs)", fontsize=30)
+    plt.ylabel("Max Positron Density", fontsize=30)
+    plt.title("Max Positron Density vs Pulse Duration", fontsize=30)
+    plt.tick_params(axis='both', which='major', labelsize=25)
+    plt.tight_layout()
+    plt.legend(loc="upper right", fontsize=25, frameon=False)
     plt.show()
