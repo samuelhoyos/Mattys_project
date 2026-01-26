94d9f8950c0d514d98d937f7ddda202dca68c4ac
Samuel Hoyos Aristizabal <samuel.hoyos@udea.edu.co>
2026-01-26 18:34:27 +0100

    Plotting minima and finding colision time

--- modules/data.py
+++ modules/data.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
+import os
 
 data_path = Path(__file__).parent.parent / "data"
 
@@ -12,22 +14,29 @@ def load_density(species: str, filename: str) -> np.array:
     S = happi.Open(f"{file_path}", verbose=False)
     species_dict = {"electrons": 1, "positrons": 0, "seed": 2}
     n = S.ParticleBinning(diagNumber=species_dict[species])
-    
+
     return np.array(n.getData())
 
+def get_collision_time(filename:str):
+    file_path = data_path / filename
+    os.environ["TFWHM_FS"] = filename.split("tfwhm")[1].split("f")[0]
+    S = happi.Open(f"{file_path}", verbose=False)
+    return (((S.namelist.v0)/(S.namelist.c_normalized+S.namelist.v0))*S.namelist.tfwhm + S.namelist.x_focus_laser/S.namelist.c_normalized)/S.namelist.omega0
+    
 def get_times(filename: str) -> np.array:
+    collision_time=get_collision_time(filename)
     file_path = data_path / filename
     S = happi.Open(f"{file_path}", verbose=False)
     times = S.Scalar("Ntot_pon").getTimes()
-    
-    return times / S.namelist.omega0
+    print(S.namelist.tfwhm_fs)
+    # z=np.array(S.Scalar("Ntot_pon").getData())
+    # indx=np.where(z>0)[0][0]+
+
+    return (times / S.namelist.omega0)-collision_time
 
 
 def get_nmax_profile(n: np.ndarray) -> np.ndarray:
     nmax_profile = np.max(n, axis=(1, 2))
-    idx = np.argmax(n)
-    t, x, y, z = np.unravel_index(idx, n.shape)
-    print(t, x, y, z)
     return nmax_profile
 
 
@@ -86,7 +95,10 @@ def fit_envelope(
     )
     return popt_long, popt_short
 
-def check_density_planes(n: np.ndarray, plane: str = "xy", index: int = 0) -> pd.DataFrame:
+
+def check_density_planes(
+    n: np.ndarray, plane: str = "xy", index: int = 0
+) -> pd.DataFrame:
     if plane == "xy":
         data_plane = n[:, :, :, index]
     elif plane == "xz":
@@ -97,3 +109,43 @@ def check_density_planes(n: np.ndarray, plane: str = "xy", index: int = 0) -> pd
         raise ValueError("Invalid plane specified. Choose from 'xy', 'xz', or 'yz'")
 
     return data_plane
+
+
+def find_n_local_minima(n_max_profile: np.ndarray, prominence: float = 0.05):
+
+    scale = np.max(n_max_profile) - np.min(n_max_profile)
+
+    inverted_profile = -n_max_profile
+    peaks, properties = find_peaks(inverted_profile, prominence=prominence * scale)
+
+    return peaks
+
+
+def average_minima_separation(
+    time_list: list,
+    minima_indices: list,
+) -> float:
+    separations = []
+    for times, minima_index in zip(
+        time_list, minima_indices
+    ):  
+        print(minima_index)
+        if len(minima_index) >= 2:
+            separation = np.mean(np.diff(times[minima_index]))
+            separations.append(separation)
+
+    if separations:
+        return np.array(separations)*1e15
+    else:
+        raise ValueError("No minima found to calculate separation.")
+    
+def get_N_pon(filename:str):
+
+    file_path = data_path / filename
+    S = happi.Open(f"{file_path}", verbose=False)
+    species_dict = {"electrons": 1, "positrons": 0, "seed": 2}
+    Npon = S.Scalar("Ntot_pon")
+ 
+    return np.array(Npon.getData())
+    
+    
--- modules/plot.py
+++ modules/plot.py
@@ -9,28 +9,47 @@ def plot_nmax_profiles(
     tfwhm_list: list,
     times_nmax_array: np.ndarray,
     nmax_array: np.ndarray,
+    linestyle: str = "-",
+    linewidth: int = 3,
+    alpha: float = 1.0,
+    legend: bool = True,
+    minima_indices: list = None,
+    nponmax_profiles: list = None,
 ):
     cmap = plt.cm.tab20
     n_curves = len(tfwhm_list)
     colors = [cmap(i) for i in range(n_curves)]
-    plt.figure(figsize=(12, 8))
+    # plt.figure(figsize=(12, 8))
     for times, nmax_profile, tfwhm in zip(times_list, nmax_profiles_list, tfwhm_list):
         plt.plot(
             times * 1e15,
             nmax_profile,
             label=rf"$t_{{\text{{fwhm}}}}={tfwhm}$fs",
             color=colors[tfwhm_list.index(tfwhm)],
+            linestyle=linestyle,
+            linewidth=linewidth,
+            alpha=alpha,
         )
+        if minima_indices is not None and nponmax_profiles is not None:
+            idx = tfwhm_list.index(tfwhm)
+            plt.plot(
+                times[minima_indices[idx]] * 1e15,
+                nponmax_profiles[idx][minima_indices[idx]],
+                "o",
+                color=colors[idx],
+                alpha=alpha,
+            )
+
     # plt.plot(
     #     times_nmax_array * 1e15, nmax_array, linestyle="-", linewidth=3, color="black"
     # )
-   
+
     plt.xlabel("$t$ (fs)", fontsize=30)
     plt.ylabel("Max Positron Density", fontsize=30)
     plt.title("Max Positron Density Profiles Over Time", fontsize=30)
-    plt.legend(loc="best", fontsize=25, frameon=False)
+    if legend:
+        plt.legend(loc="best", fontsize=25, frameon=False)
     plt.tight_layout()
-    plt.show()
 
 
 def plot_single_profile(times, nmax_profile, tfwhm):
@@ -54,40 +73,48 @@ def plot_nmax_tfwhm(
     nmax_array: np.ndarray,
     popt_long: np.ndarray = None,
     popt_short: np.ndarray = None,
+    linestyle: str = "-.",
+    linewidth: int = 3,
+    marker: str = "o",
+    alpha: float = 1.0,
+    binning: int = 128,
 ):
 
     mask = list(nmax_array).index(np.max(nmax_array))
     tfwhm_list_short = np.array(tfwhm_list)[:mask]
     tfwhm_list_long = np.array(tfwhm_list)[mask:]
     print(tfwhm_list_long)
-    plt.figure(figsize=(12, 8))
+
     plt.plot(
         list(tfwhm_list),
         nmax_array,
-        marker="o",
-        linestyle="",
+        marker=marker,
+        linestyle="-",
         color="blue",
-        label="Data",
+        label=f"Data for binning {binning}",
+        alpha=alpha,
     )
-    if popt_long is not None:
+    # if popt_long is not None:
 
-        plt.plot(
-            tfwhm_list_long,
-            data.long_time(tfwhm_list_long, *popt_long),
-            linestyle="--",
-            linewidth=3,
-            color="red",
-            # label=f'${popt_long[0]}/(t_{{fwhm}}-{popt_long[1]})+{{{popt_long[2]}}}$'
-        )
-    if popt_short is not None:
-        plt.plot(
-            tfwhm_list_short,
-            data.short_time(tfwhm_list_short, *popt_short),
-            linestyle="--",
-            linewidth=3,
-            color="red",
-            # label=f'${popt_short[0]:.2f}+{popt_short[1]:.2f}t_{{fwhm}}$'
-        )
+    #     plt.plot(
+    #         tfwhm_list_long,
+    #         data.long_time(tfwhm_list_long, *popt_long),
+    #         linestyle=linestyle,
+    #         linewidth=linewidth,
+    #         color="red",
+    #         alpha=alpha,
+    #         # label=f'${popt_long[0]}/(t_{{fwhm}}-{popt_long[1]})+{{{popt_long[2]}}}$'
+    #     )
+    # if popt_short is not None:
+    #     plt.plot(
+    #         tfwhm_list_short,
+    #         data.short_time(tfwhm_list_short, *popt_short),
+    #         linestyle=linestyle,
+    #         linewidth=linewidth,
+    #         color="red",
+    #         alpha=alpha,
+    #         # label=f'${popt_short[0]:.2f}+{popt_short[1]:.2f}t_{{fwhm}}$'
+    #     )
 
     plt.xlabel("$t_{fwhm}$ (fs)", fontsize=30)
     plt.ylabel("Max Positron Density", fontsize=30)
@@ -95,4 +122,10 @@ def plot_nmax_tfwhm(
     plt.tick_params(axis="both", which="major", labelsize=25)
     plt.tight_layout()
     plt.legend(loc="upper right", fontsize=25, frameon=False)
-    plt.show()
+
+def N_pon(N_pon:np.array, times:np.array, color:str):
+    
+    plt.plot(times*1e15, N_pon, color=color)
+    
+
+
